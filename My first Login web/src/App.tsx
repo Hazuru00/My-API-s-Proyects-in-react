17d93c4ab482300895e237691701bfe403f4e37b
Moises Marcano <92418515+Hazuru00@users.noreply.github.com>
2025-10-23 15:12:26 +0000

    corrige la clase del div principal para aplicar estilos de fondo correctamente

--- My first Login web/src/App.tsx
+++ My first Login web/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="bg-crimson w-full h-screen flex flex-col justify-center items-center">
         <h1>My First Login Web</h1>
         <p>Welcome to my first login web application built with React! xd</p>
       </div>
