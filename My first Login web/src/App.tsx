c09ac1e16082c050a2e9a1b28be4f59da9210df1
Moises Marcano <92418515+Hazuru00@users.noreply.github.com>
2025-10-23 16:40:23 +0000

    feat: agregar componente TrueFocus con animaciones y enfoque dinámico

--- My first Login web/src/App.tsx
+++ My first Login web/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { TrueFocus } from './components/TrueFocus.jsx'
+import TrueFocus from './components/TrueFocus';
 import { BeakerIcon } from '@heroicons/react/24/solid'
 
 function App() {
@@ -9,6 +9,7 @@ function App() {
       <div className="bg-crimson w-full h-screen flex flex-col justify-center items-center">
         <h1>My First Login Web</h1> <BeakerIcon className="size-6 text-blue-500" />
         <p>Welcome to my first login web application built with React! xd</p>
+        <TrueFocus sentence="True Focus" manualMode={true} blurAmount={5} borderColor="red" animationDuration={2} pauseBetweenAnimations={1}/>
       </div>
     </>
   )
