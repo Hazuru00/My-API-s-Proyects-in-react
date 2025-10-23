218bd187787854dd6b9516d99e821277584b53c0
Moises Marcano <92418515+Hazuru00@users.noreply.github.com>
2025-10-23 15:26:30 +0000

    probando librerias de tailwind css y agregando mas estilos temporales

--- My first Login web/src/App.tsx
+++ My first Login web/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
+import { BeakerIcon } from '@heroicons/react/24/solid'
 
 function App() {
 
   return (
     <>
       <div className="bg-crimson w-full h-screen flex flex-col justify-center items-center">
-        <h1>My First Login Web</h1>
+        <h1>My First Login Web</h1> <BeakerIcon className="size-6 text-blue-500" />
         <p>Welcome to my first login web application built with React! xd</p>
       </div>
     </>
