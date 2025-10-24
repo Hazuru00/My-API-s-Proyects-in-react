4f788e6bce9bb0d90fb9772c3d365f1fff9ab3d8
Moises Marcano <92418515+Hazuru00@users.noreply.github.com>
2025-10-23 23:38:58 -0400

    solucion de errores en componentes y interfaz mejorada pa

--- My first Login web/src/App.tsx
+++ My first Login web/src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css'
+import { ModeToggle } from './components/mode-toggle';
 import { ThemeProvider } from "./components/theme-provider"
 import TrueFocus from './components/TrueFocus';
+
   //import { BeakerIcon } from '@heroicons/react/24/solid'
   // <BeakerIcon className=''/>
 function App() {
@@ -8,15 +10,20 @@ function App() {
   return (
     <>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        {children}
+        {
+        <>
+        <nav className='bg-gray-800 p-4'>
+          <ModeToggle />
+        </nav>
+          <div>
+            <div className="bg-crimson w-full h-screen flex flex-col justify-center items-center">
+              <TrueFocus sentence="Mi Primer Login" manualMode={true} blurAmount={5} borderColor="#d62abf" animationDuration={0.4} pauseBetweenAnimations={1}/><br/>
+              <p>Welcome to my first login web application built with React! xd</p>
+            </div>
+          </div>
+        </>
+        }
       </ThemeProvider>
-
-      <div>
-        <div className="bg-crimson w-full h-screen flex flex-col justify-center items-center">
-          <TrueFocus sentence="Mi Primer Login" manualMode={true} blurAmount={5} borderColor="#d62abf" animationDuration={0.4} pauseBetweenAnimations={1}/><br/>
-          <p>Welcome to my first login web application built with React! xd</p>
-        </div>
-      </div>
     </>
   )
 }
