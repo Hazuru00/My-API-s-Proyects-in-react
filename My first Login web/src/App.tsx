7e027e1059d96109e5d0ba503bfa336129f79361
Moises Marcano <92418515+Hazuru00@users.noreply.github.com>
2025-10-24 00:49:46 -0400

    feat: agregar componente LetterGlitch para efectos de texto animados

--- My first Login web/src/App.tsx
+++ My first Login web/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { ModeToggle } from './components/mode-toggle';
 import { ThemeProvider } from "./components/theme-provider"
 import TrueFocus from './components/TrueFocus';
+import LetterGlitch from './components/LetterGlitch.tsx';
 
   //import { BeakerIcon } from '@heroicons/react/24/solid'
   // <BeakerIcon className=''/>
@@ -12,12 +13,14 @@ function App() {
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         {
         <>
-        <nav className='bg-gray-800 p-4'>
+        <nav className='bg-gray-900 p-4'>
+          <LetterGlitch glitchColors={ ['#2b4539', '#61dca3', '#61b3dc'] } glitchSpeed={50} centerVignette={true} outerVignette={false} smooth={true} characters='ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$&*()-_+=/[]{};:<>.,0123456789'/>
+
           <ModeToggle />
         </nav>
           <div>
             <div className="bg-crimson w-full h-screen flex flex-col justify-center items-center">
-              <TrueFocus sentence="Mi Primer Login" manualMode={true} blurAmount={5} borderColor="#d62abf" animationDuration={0.4} pauseBetweenAnimations={1}/><br/>
+              <TrueFocus sentence="Mi Primer Login" manualMode={false} blurAmount={5} borderColor="pink-500" animationDuration={0.4} pauseBetweenAnimations={1}/><br/>
               <p>Welcome to my first login web application built with React! xd</p>
             </div>
           </div>
